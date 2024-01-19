0a3e30ee1985b14640baff5e6d2daff2bfebb6d1
Kelsey De Busschere <kelseydebusschere@gmail.com>
2024-01-19 09:48:48 +0100

    Connect to database

--- core/DatabaseManager.php
+++ core/DatabaseManager.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'config.php';
+require_once 'root/config.php';
 
 class DatabaseManager 
 {
--- index.php
+++ index.php
@@ -6,11 +6,19 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+//include database stuff
+require_once 'root/config.php';
+require 'core/DatabaseManager.php';
 //include all your model files here
 require 'Model/Article.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
 require 'Controller/ArticleController.php';
+
+
+//database connection
+$databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
+$databaseManager->connect();
 
 // Get the current page to load
 // If nothing is specified, it will remain empty (home should be loaded)
@@ -24,6 +32,7 @@ switch ($page) {
         // $articleController = new ArticleController;
         // $articleController->index();
         (new ArticleController())->index();
+
         break;
     case 'articles-show':
         // TODO: detail page
